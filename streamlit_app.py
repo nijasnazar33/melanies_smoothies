b4e362c0a50e01998762ed3c5156b78fada5c76a
Nijas <nijasnazar33@gmail.com>
2025-11-03 20:33:05 +0530

    Add Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
@@ -29,7 +30,12 @@ session = cnx.session()
 
 # my_dataframe = session.table('smoothies.public.fruit_options') #to select full table
 my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True) #to display dataframe as table
+# st.dataframe(data=my_dataframe, use_container_width=True) #to display dataframe as table
+# st.stop()
+
+#Convert the Snowpart Dataframe to a Pandas dataframe so that we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
